818dae7b96a5d72c8650152b90b9285661b43b70
Lucélio <119617166+Lucelioo@users.noreply.github.com>
2024-11-05 11:54:48 -0300

    Update main_loja.py

--- main_loja.py
+++ main_loja.py
@@ -1,16 +1,25 @@
 import classes_DB_loja
 import os
+from funcoes_loja import MenuPrincipal
 
-# limpa tela
-os.system("cls" if os.name == "nt" else "clear")
+def TelaInicial():
+    # Limpa a tela
+    os.system("cls" if os.name == "nt" else "clear")
 
-# Tela principal
-print("\t","="*48)
-print("\t | BEM VINDO AO SISTEMA DE CADASTRO DE PRODUTOS |")
-print("\t","="*48)
-print("")
-usuario = input("Login do sistema: ")
-senha = input("Senha do sistema: ")
+    # Tela principal
+    print("\t", "="*48)
+    print("\t | BEM VINDO AO SISTEMA DE CADASTRO DE PRODUTOS |")
+    print("\t", "="*48)
+    print("")
+    usuario = input("Login do sistema: ")
+    senha = input("Senha do sistema: ")
 
-# Valida usuario e senha com o banco de dados
-classes_DB_loja.db.VerificaUsuario(usuario,senha)
+    # Valida usuario e senha com o banco de dados
+    if classes_DB_loja.db.VerificaUsuario(usuario, senha):
+        MenuPrincipal()
+    else:
+        print("Falha na autenticação. Verifique seu login e senha.")
+        input("Pressione enter para tentar novamente...")
+        TelaInicial()
+
+TelaInicial()
